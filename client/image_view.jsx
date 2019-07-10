6d1cb8967c54eabf23f78cfff7df31a57a953353
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2019-07-10 20:41:36 +0300

    /image/:iid simplify

--- client/image_view.jsx
+++ client/image_view.jsx
@@ -1,8 +1,7 @@
-/* global React, Cookies, ReachRouter */
+/* global React, ReachRouter */
 
-let {Link, navigate} = ReachRouter
+let {Link} = ReachRouter
 import * as u from './u.js'
-import * as ic from './image_common.js'
 
 export default class Upload extends React.Component {
     constructor(props) {
@@ -10,7 +9,9 @@ export default class Upload extends React.Component {
 	this.state = {}
 
 	u.fetch_json(`/api/image/view?iid=${this.iid()}`).then( json => {
-	    this.setState({image: json})
+	    let r = Object.assign({}, json[0])
+	    r.tags = json.map( v => v.tag).join`, `
+	    this.setState(r)
 	}).catch( e => {
 	    this.setState({error: e.message})
 	})
@@ -22,62 +23,55 @@ export default class Upload extends React.Component {
 	      <div className="form-error">{this.state.error}</div>
 
 	      <div className={this.state.error ? 'hidden' : ''}>
-		<h1>{this.img().title}</h1>
+		<h1>{this.state.title}</h1>
 		<div id="image--viewer">
 		  <a id="image--viewer__img"
-		     href={iid2image(this.img().uid, this.img().iid).svg}>
-		    <img src={this.state.image &&
-			 iid2image(this.img().uid, this.img().iid).thumbnail} />
+		     href={this.img().svg} target="_blank"
+		     rel="noopener noreferrer">
+		    <img src={this.state.iid && this.img().thumbnail} />
 		  </a>
 
 		  <span>Uploader:</span>
 		  <span>
-		    <Link to={"/user/" + this.img().uid}>{this.img().user_name}</Link>
-
+		    <Link to={"/user/" + this.state.uid}>{this.state.user_name}</Link>
 		  </span>
 
 		  <span>License:</span>
-		  <span>{this.img().license}</span>
+		  <span>{this.state.license}</span>
 
 		  <span>Original filename:</span>
-		  <code>{this.img().filename}</code>
+		  <code>{this.state.filename}</code>
 
 		  <span>mtime:</span>
-		  <span>{u.date_fmt(this.img().mtime)}</span>
+		  <span>{u.date_fmt(this.state.mtime)}</span>
 
 		  <span>Size:</span>
-		  <span>{this.img().size} bytes</span>
-
-		  <span>Description:</span>
-		  <div>{this.img().desc}</div>
+		  <span>{this.state.size} bytes</span>
 
 		  <span>Tags:</span>
-		  <div>{this.tags()}</div>
+		  <div>{this.state.tags}</div>
+
+		  <span>Description:</span>
+		  <div>{this.state.desc}</div>
+
 		</div>
 	      </div>
 	    </>
 	)
     }
 
-    img() {
-	return this.state.image ? this.state.image[0] : {}
-    }
-
-    tags() {
-	if (!this.state.image) return
-	return this.state.image.map( v => v.tag).join`, `
-    }
-
     iid() {
 	let p = window.location.href.split('/')
 	return p[p.length - 1] || -1
     }
-}
 
-function iid2image(uid, iid) {
-    let dir = '/clipart'
-    return {
-	svg: [dir, 'images', uid, `${iid}.svg`].join('/'),
-	thumbnail: [dir, 'thumbnails', uid, `${iid}.png`].join('/')
+    img() {
+	let dir = '/clipart'
+	return {
+	    svg: [dir, 'images', this.state.uid,
+		  `${this.state.iid}.svg`].join('/'),
+	    thumbnail: [dir, 'thumbnails', this.state.uid,
+			`${this.state.iid}.png`].join('/')
+	}
     }
 }
